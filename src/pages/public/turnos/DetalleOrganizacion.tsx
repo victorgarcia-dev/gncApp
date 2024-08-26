8f267e0a485e517c41503c0449a3fcdceceff57d
Pablo David Villegas <pvillegas@koandina.com>
2024-08-26 17:47:01 -0300

    subo mas cambios

--- src/pages/public/turnos/DetalleOrganizacion.tsx
+++ src/pages/public/turnos/DetalleOrganizacion.tsx
@@ -26,15 +26,22 @@ export const DetalleOrganizacion = () => {
             <ul role="list" className="divide-y divide-gray-100">
             {organizaciones.map((org:any) => (
             <li key={org.id} className="flex justify-between gap-x-6 py-5">
-                <div className="flex min-w-0 gap-x-4">
-            <img alt=""  className="h-12 w-12 flex-none rounded-full bg-gray-50" />
-            <div className="min-w-0 flex-auto">
-              <p className="text-sm font-semibold leading-6 text-gray-900">{org.nombre}</p>
-              <p className="mt-1 truncate text-xs leading-5 text-gray-500">{org.direccion}</p>
-            </div>
-          </div>
+                <div className="mt-1 flex items-center gap-x-1.5">
+                <div className="flex-none rounded-full bg-emerald-500/20 p-1">
+                  <div className="h-1.5 w-1.5 rounded-full bg-emerald-500" />
+                </div>
+                <p className="text-xs leading-5 text-gray-500">9:00 hs - Disponible</p>
+              </div>
             </li>
             ))}
+            <li className="flex justify-between gap-x-6 py-5">
+                <div className="mt-1 flex items-center gap-x-1.5">
+                <div className="flex-none rounded-full bg-red-500/20 p-1">
+                  <div className="h-1.5 w-1.5 rounded-full bg-red-500" />
+                </div>
+                <p className="text-xs leading-5 text-gray-500">9:00 hs - No Disponible</p>
+              </div>
+            </li>
             </ul>
     </div>
   );
